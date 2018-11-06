a80245b41311918591019108c6c91b4396b57567
Rafael Gonçalves Barreira <rafaelgoncalvesbarreira@gmail.com>
2018-11-06 17:35:57 -0300

    load info and tests

--- irrf.py
+++ irrf.py
@@ -9,7 +9,34 @@ class IrrfRate:
         self.dependents_reduction = dependents_reduction
     
     def calculate(self, base_value, dependents=0):
-        base = base_value if dependents==0 else base_value - (dependents * self.dependents_reduction)
-        rate = base * self.rate / 100
-        value = rate - self.deduction
+        value = 0
+        if self.min <= base_value <= self.max:
+            base = base_value if dependents==0 else base_value - (dependents * self.dependents_reduction)
+            rate = base * self.rate / 100
+            value = rate - self.deduction
         return value
+
+def load_dependent_reduction(csv_file_path='dependents.csv'):
+    reduction = 0
+    with open(csv_file_path) as csvfile:
+        lines = csvfile.readlines()
+        reduction = float(lines[1])
+    return reduction
+
+def load_irrf(csv_file_path='irrf.csv', dependents_reduction=0):
+    field_names=['min', 'max', 'rate', 'deduction']
+    irrfs=[]
+    with open(csv_file_path) as csvfile:
+        reader = csv.DictReader(csvfile, fieldnames=field_names)
+        for row in reader:
+            o = IrrfRate(row['min'], row['max'], row['rate'], row['deduction'], dependents_reduction)
+            irrfs.append(o)
+    return irrfs
+
+
+def get_irrf_by_value(base_irrf, list_irrf):
+    list_irrf.sort(key= lambda x: x.rate)
+    for irff in list_irrf:
+        if irff.min <= base_irrf <= irff.max:
+            return irff
+    return list_irrf[-1]
--- tests/test_irrf.py
+++ tests/test_irrf.py
@@ -8,6 +8,11 @@ class TestIrrf(unittest.TestCase):
         irrf_value = irrf_rate.calculate(1500)
         self.assertEqual(irrf_value,0)
     
+    def test_wrongRating(self):
+        irrf_rate = IrrfRate(1800,3500,10,100,100)
+        irrf_value = irrf_rate.calculate(1500)
+        self.assertEqual(irrf_value,0)
+    
     def test_payTaxWithoutDependent(self):
         irrf_rate = IrrfRate(1800,3500,10,100,100)
         irrf_value = irrf_rate.calculate(3000)
@@ -17,3 +22,19 @@ class TestIrrf(unittest.TestCase):
         irrf_rate = IrrfRate(1800,3500,10,100,100)
         irrf_value = irrf_rate.calculate(3000,1)
         self.assertEqual(irrf_value, 190)
+
+class TestLoadInss(unittest.TestCase):
+    def setUp(self):
+        self.table = [
+            IrrfRate(0,1800,0,0,100),
+            IrrfRate(1801,3500,10,100,100),
+            IrrfRate(3501,999999,20,300,100)
+        ]
+    
+    def test_load_anyone(self):
+        irrf_rate = irrf.get_irrf_by_value(3000,self.table)
+        self.assertEqual(irrf_rate.rate, 10)
+
+
+if __name__ == "__main__":
+    unittest.main()
